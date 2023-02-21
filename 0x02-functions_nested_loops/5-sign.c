1f865d0288887cdb8b2087ed1b06d2523df08409
Oluwanifemi <adenugamn@gmail.com>
2023-02-21 06:00:50 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive numbers, -1 for negative numbers and 0 for anything else
+ * Return: 1 for positive num, -1 for negative numb and 0 for anything else
  */
 
 int print_sign(int n)
