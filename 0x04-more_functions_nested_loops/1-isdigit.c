a2920cdf4725c80084c18f3d39b68777ce843833
Oluwanifemi <adenugamn@gmail.com>
2023-02-23 04:25:25 -0800

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,17 +2,16 @@
 
 /**
  * _isdigit - checks if a character is a digit
- * @c: the character to be checked
- *
+ * @x: the character to be checked
  * Return: 1 if the character is a digit, 0 otherwise
  */
 
 int _isdigit(int x)
 
 {
-    if (x >= 48 && x <= 57)
-    {
-        return (1);
-    }
-    return (0);
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
 }
